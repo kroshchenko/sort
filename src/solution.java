7e335ab25576b1bc1e2c03ee7aa7782d4227c778
kroshchenko <44199648+kroshchenko@users.noreply.github.com>
2018-10-17 11:31:45 +0300

    Update solution.java

--- src/solution.java
+++ src/solution.java
@@ -3,6 +3,8 @@ public class solution {
     public static void main(String[] args) {
         System.out.println('H');
         System.out.println('S');
-        System.out.println('S');
+        System.out.println('H');
+        System.out.println('H');
+        System.out.println('H');
     }
 }
